e451d315b246e15921c573f194b4add2cd769f40
Nicholas Komick <nicholas.komick@dfo-mpo.gc.ca>
2016-02-11 13:32:17 -0800

    modify database file name to reflect that it includes 2013 and 2014

--- config/2013_report_config.r
+++ config/2013_report_config.r
@@ -1,7 +1,7 @@
 
 run.year <- 2013
 
-post.season.fram.db <- "./fram db/FramVS2-PSC-Coho-Backwards-for 2013 working file.mdb"
+post.season.fram.db <- "./fram db/FramVS2-PSC-Coho-Backwards-for 2013 and 2014.mdb"
 post.season.run.name <- "bk 2013 Final ver3 Feb 11 2015"
 
 
--- config/2014_report_config.r
+++ config/2014_report_config.r
@@ -1,9 +1,9 @@
 
 run.year <- 2014
 
-post.season.fram.db <- "./fram db/2015PFMC_NOF_CohoFRAMdatabase_ar_work2.mdb"
-post.season.run.name <- "bc-test2 OF bc-1416 Final"
+post.season.fram.db <- "./fram db/FramVS2-PSC-Coho-Backwards-for 2013 and 2014.mdb"
+post.season.run.name <- "bc-BK2014 w TAMM inputs final#2"
 
 
 pre.season.fram.db <- "./fram db/Fram PFMC Pre 2015.mdb"
-pre.season.run.name <- "bc-1416 Final w TAMM inputs"
+pre.season.run.name <- "bc-Coho1418 US and BC preseason 2014"
