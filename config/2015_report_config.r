1032bb6c108e9d6f62159cdccc83e6caee925956
Nicholas Komick <nicholas.komick@dfo-mpo.gc.ca>
2017-02-15 10:02:25 -0800

    Configure 2015 post season report

--- config/2015_report_config.r
+++ config/2015_report_config.r
@@ -1,9 +1,9 @@
 
 run.year <- 2015
 
-post.season.fram.db <- "./fram db/CohoFRAMVB2015Pre&PostNew.mdb"
-post.season.run.name <- "bkCoho2015Post"
-post.season.tamm <- "./fram db/Coho1523FinalwAttachCrevised and updated.xlsm"
+post.season.fram.db <- "./fram db/FramVS2-PSC-Coho-Backwards-for 2015.mdb"
+post.season.run.name <- "bc-bkCoho2015 fw catch queets"
+post.season.tamm <- "./fram db/coho BK 2015 in process no tami step 3.xlsm"
 
 pre.season.fram.db <- "./fram db/CohoFRAMVB2015Pre&PostNew.mdb"
 pre.season.run.name <- "bc-Coho1523 Final"
