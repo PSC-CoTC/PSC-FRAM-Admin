9e7fb6f95ccb329ff97a61cc49639a4491536161
Nicholas Komick <nicholas.komick@dfo-mpo.gc.ca>
2017-02-16 10:59:55 -0800

    Simplify the database name for inclusion in report
    The database should focus on having the intent of the runs (e.g.
    pre/post season) and NOT have year specific databases

--- config/2015_report_config.r
+++ config/2015_report_config.r
@@ -1,10 +1,10 @@
 
 run.year <- 2015
 
-post.season.fram.db <- "./fram db/FramVS2-PSC-Coho-Backwards-for 2013 and 2014.mdb"
+post.season.fram.db <- "./fram db/Final pre and post databases/FramVS2-PSC-Coho-PostSeason.mdb"
 post.season.run.name <- "bc-bkCoho2015 Final"
 post.season.tamm <- "./fram db/coho BK 2015 Final Feb 15th.xlsm"
 
-pre.season.fram.db <- "./fram db/CohoFRAMVB2015Pre&PostNew.mdb"
+pre.season.fram.db <- "./fram db/Final pre and post databases/FramVS2-PSC-Coho-PreSeason.mdb"
 pre.season.run.name <- "bc-Coho1523 Final"
 pre.season.tamm <- "./fram db/Coho1523FinalwAttachCrevised and updated.xlsm"
