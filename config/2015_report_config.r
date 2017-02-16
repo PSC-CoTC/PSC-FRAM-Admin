c81bace1bd958f20d0f2354c778d63239469d9e6
Nicholas Komick <nicholas.komick@dfo-mpo.gc.ca>
2017-02-16 09:11:32 -0800

    configure for 2015 annual report

--- config/2015_report_config.r
+++ config/2015_report_config.r
@@ -1,9 +1,9 @@
 
 run.year <- 2015
 
-post.season.fram.db <- "./fram db/FramVS2-PSC-Coho-Backwards-for 2015.mdb"
-post.season.run.name <- "bc-bkCoho2015 fw catch queets"
-post.season.tamm <- "./fram db/coho BK 2015 in process no tami step 3.xlsm"
+post.season.fram.db <- "./fram db/FramVS2-PSC-Coho-Backwards-for 2013 and 2014.mdb"
+post.season.run.name <- "bc-bkCoho2015 Final"
+post.season.tamm <- "./fram db/coho BK 2015 Final Feb 15th.xlsm"
 
 pre.season.fram.db <- "./fram db/CohoFRAMVB2015Pre&PostNew.mdb"
 pre.season.run.name <- "bc-Coho1523 Final"
