1851673cccc668d9965cf095d71a90119dbe8df0
Nicholas Komick <nicholas.komick@dfo-mpo.gc.ca>
2017-03-01 11:49:30 -0800

    Add sample configs to export past FRAM years

--- config/export_fram_fishery_config.r
+++ config/export_fram_fishery_config.r
@@ -1,6 +1,31 @@
 
-
+###########   2015 Post Season Catch #################
 fram.db.name <- "./fram db/Final pre and post databases/FramVS2-PSC-Coho-PostSeason.mdb"
 fram.run.name <- "bc-bkCoho2015 Final"
-
 run.year <- 2015
+
+###########   2014 Post Season Catch #################
+#fram.db.name <- "./fram db/FramVS2-PSC-Coho-Backwards-for 2013 and 2014.mdb"
+#fram.run.name <- "bc-BK2014 w TAMM inputs final#2"
+#run.year <- 2014
+
+###########   2013 Post Season Catch #################
+#fram.db.name <- "./fram db/FramVS2-PSC-Coho-Backwards-for 2013 and 2014.mdb"
+#fram.run.name <- "bk 2013 Final ver3 Feb 11 2015"
+#run.year <- 2013
+
+
+###########   2012 Post Season Catch #################
+#fram.db.name <- "./fram db/FramVS2-PSC-Coho-Backwards-2013.mdb"
+#fram.run.name <- "Coho2012Post_PSC SSNPx2 Q 2014"
+#run.year <- 2012
+
+###########   2011 Post Season Catch #################
+#fram.db.name <- "./fram db/FramVS2-PSC-Coho-Backwards-2012 Feb 12.mdb"
+#fram.run.name <- "Coho2011Post_PSC 2013"
+#run.year <- 2011
+
+###########   2010 Post Season Catch #################
+#fram.db.name <- "./fram db/FramVS2-PSC-Coho-Backwards-2012 Feb 12.mdb"
+#fram.run.name <- "bk10PSCFeb14"
+#run.year <- 2010
