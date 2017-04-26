06ec123d999c581e456afbb1731c71c5bd604743
Nicholas Komick <nicholas.komick@dfo-mpo.gc.ca>
2017-04-26 15:11:45 -0700

    rename variables in sql to simplify variable names

--- sql/FramBackwardEscapement.sql
+++ sql/FramBackwardEscapement.sql
@@ -1,8 +1,8 @@
 SELECT RunID.RunID as "fram_run_id", 
   RunID.RunYear as "run_year",
   BackwardsFRAM.StockID as "fram_stock_id", 
-  BackwardsFRAM.TargetFlag as "fram_escapement_flag",
-  BackwardsFRAM.TargetEscAge3 AS "fram_escapement"
+  BackwardsFRAM.TargetFlag as "escapement_flag",
+  BackwardsFRAM.TargetEscAge3 AS "target_escapement"
 FROM (BackwardsFRAM INNER JOIN RunID ON BackwardsFRAM.RunID = RunID.RunID) 
 WHERE RunID.RunName = %RUNNAME%;
 
