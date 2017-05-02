5fbcc54a369737b9ebbd60f9ec5fc12e840bcdf3
Nicholas Komick <nicholas.komick@dfo-mpo.gc.ca>
2017-05-02 11:07:36 -0700

    add pre and post season model runs

--- config/create_import_config.r
+++ config/create_import_config.r
@@ -1,4 +1,86 @@
 
-fram.db.name <- "./fram db/CohoFRAMVB2015Pre&PostNew.mdb"
-fram.run.name <- "bkCoho2015Post"
+#fram.db.name <- "./fram db/CohoFRAMVB2015Pre&PostNew.mdb"
+#fram.run.name <- "bkCoho2015Post"
 
+###########   2015 Post Season Catch #################
+#fram.db.name <- "./fram db/Final pre and post databases/FramVS2-PSC-Coho-PostSeason.mdb"
+#fram.run.name <- "bc-bkCoho2015 Final"
+#run.year <- 2015
+
+###########   2014 Post Season Catch #################
+#fram.db.name <- "./fram db/FramVS2-PSC-Coho-Backwards-for 2013 and 2014.mdb"
+#fram.run.name <- "bc-BK2014 w TAMM inputs final#2"
+#run.year <- 2014
+
+###########   2013 Post Season Catch #################
+#fram.db.name <- "./fram db/FramVS2-PSC-Coho-Backwards-for 2013 and 2014.mdb"
+#fram.run.name <- "bk 2013 Final ver3 Feb 11 2015"
+#run.year <- 2013
+
+
+###########   2012 Post Season Catch #################
+#fram.db.name <- "./fram db/FramVS2-PSC-Coho-Backwards-2013.mdb"
+#fram.run.name <- "Coho2012Post_PSC SSNPx2 Q 2014"
+#run.year <- 2012
+
+###########   2011 Post Season Catch #################
+#fram.db.name <- "./fram db/FramVS2-PSC-Coho-Backwards-2012 Feb 12.mdb"
+#fram.run.name <- "Coho2011Post_PSC 2013"
+#run.year <- 2011
+
+###########   2010 Post Season Catch #################
+fram.db.name <- "./fram db/FramVS2-PSC-Coho-Backwards-2012 Feb 12.mdb"
+fram.run.name <- "bk10PSCFeb14"
+run.year <- 2010
+
+###########   2009 Post Season Catch #################
+#fram.db.name <- "./fram db/FramVS2-PSC-Coho-Backwards-2012 Feb 12.mdb"
+#fram.run.name <- "BK09 New CNR"
+#run.year <- 2009
+
+###########   2008 Post Season Catch #################
+#fram.db.name <- "./fram db/FramVS2-PSC-Coho-Backwards-2012 Feb 12.mdb"
+#fram.run.name <- "BK08.cmd"
+#run.year <- 2008
+
+
+###########   2015 Pre Season Catch #################
+#fram.db.name <- "./fram db/Final pre and post databases/FramVS2-PSC-Coho-PreSeason.mdb"
+#fram.run.name <- "bc-Coho1523 Final"
+#run.year <- 2015
+
+###########   2014 Pre Season Catch #################
+#fram.db.name <- "./fram db/Final pre and post databases/FramVS2-PSC-Coho-PreSeason.mdb"
+#fram.run.name <- "bc-Coho1418 US and BC preseason 2014"
+#run.year <- 2014
+
+###########   2013 Pre Season Catch #################
+#fram.db.name <- "./fram db/Final pre and post databases/FramVS2-PSC-Coho-PreSeason.mdb"
+#fram.run.name <- "bc-1323"
+#run.year <- 2013
+
+
+###########   2012 Pre Season Catch #################
+#fram.db.name <- "./fram db/Final pre and post databases/FramVS2-PSC-Coho-PreSeason.mdb"
+#fram.run.name <- "1229.cmd"
+#run.year <- 2012
+
+###########   2011 Pre Season Catch #################
+#fram.db.name <- "./fram db/Final pre and post databases/FramVS2-PSC-Coho-PreSeason.mdb"
+#fram.run.name <- "1116.cmd"
+#run.year <- 2011
+
+###########   2010 Post Season Catch #################
+#fram.db.name <- "./fram db/Final pre and post databases/FramVS2-PSC-Coho-PreSeason.mdb"
+#fram.run.name <- "1016.cmd"
+#run.year <- 2010
+
+###########   2009 Pre Season Catch #################
+#fram.db.name <- "./fram db/Final pre and post databases/FramVS2-PSC-Coho-PreSeason.mdb"
+#fram.run.name <- "0921.cmd"
+#run.year <- 2009
+
+###########   2008 Pre Season Catch #################
+#fram.db.name <- "./fram db/Final pre and post databases/FramVS2-PSC-Coho-PreSeason.mdb"
+#fram.run.name <- "0824.cmd"
+#run.year <- 2008
