ab8898128f65a4c922cde87a696d26c861546a88
methiess <m_thiess@hotmail.com>
2018-01-30 14:41:40 -0800

    Add files via upload
    Added 2016_report_config.r

---
+++ config/2016_report_config.r
@@ -0,0 +1,15 @@
+
+run.year <- 2016
+
+post.season.fram.db <- "./fram db/Final pre and post databases/2018PFMC_NOF_ForPSC-Coho-Backwards-thru2016_compact.mdb"
+post.season.run.name <- "bc-Coho1637 Final + BP27"
+post.season.tamm <- "./fram db/TAMM_Files_Postseason/coho BK 2015 Final Feb 15th.xlsm"
+
+pre.season.fram.db <- "./fram db/Final pre and post databases/2018PFMC_NOF_ForPSC-Coho-PreSeason-thru2016_compact.mdb"
+pre.season.run.name <- "bc-Coho1637 Final"
+#pre.season.tamm <- "./fram db/TAMM_Files_Preseason/Coho1637_FinalPFMCOcean_MayNOF_052616rv_wMultiYrFix.xlsm"
+pre.season.tamm <- "./fram db/TAMM_Files_Postseason/coho BK 2015 Final Feb 15th.xlsm"
+
+
+#Need to get an original copy of the PreSeason db and figure out the preseason TAMM (doesn't work; check TammFisheryRef.csv 
+#and/or TammData.r)
