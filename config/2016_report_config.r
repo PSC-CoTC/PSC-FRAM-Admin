168706c14639f14c559f2c8b5fd988c90aba4c91
methiess <m_thiess@hotmail.com>
2018-01-30 23:12:34 +0000

    Update 2016_report_config.r
    Specify pre-season TAMM for 2016

--- config/2016_report_config.r
+++ config/2016_report_config.r
@@ -7,9 +7,5 @@ post.season.tamm <- "./fram db/TAMM_Files_Postseason/coho BK 2015 Final Feb 15th
 
 pre.season.fram.db <- "./fram db/Final pre and post databases/2018PFMC_NOF_ForPSC-Coho-PreSeason-thru2016_compact.mdb"
 pre.season.run.name <- "bc-Coho1637 Final"
-#pre.season.tamm <- "./fram db/TAMM_Files_Preseason/Coho1637_FinalPFMCOcean_MayNOF_052616rv_wMultiYrFix.xlsm"
-pre.season.tamm <- "./fram db/TAMM_Files_Postseason/coho BK 2015 Final Feb 15th.xlsm"
-
-
-#Need to get an original copy of the PreSeason db and figure out the preseason TAMM (doesn't work; check TammFisheryRef.csv 
-#and/or TammData.r)
+pre.season.tamm <- "./fram db/TAMM_Files_Preseason/Coho1637_FinalPFMCOcean_MayNOF_052616rv_wMultiYrFix.xlsm"
+#pre.season.tamm <- "./fram db/TAMM_Files_Postseason/coho BK 2015 Final Feb 15th.xlsm"  # this works to produce annual report
