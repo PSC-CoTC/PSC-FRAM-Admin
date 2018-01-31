e0a5b1c2d5415983e51b20771fcffdc6228da7fb
Nicholas Komick <nicholas.komick@dfo-mpo.gc.ca>
2018-01-31 15:45:33 -0800

    Fix security issue with RScript
    CDFO computers block RScript command from accessing to some files, which
    caused knitr to fail because it could not access HTML template file.
    Fixed the issue by using the standard R applicaiton.

--- 2015_Create_Annual_Report.bat
+++ 2015_Create_Annual_Report.bat
@@ -5,7 +5,7 @@ call ./bin/setup
 set ANNUAL_REPORT_LOG=".\log\2015_Annual_Report.log"
 set ANNUAL_REPORT_CONFIG=".\config\2015_report_config.r"
 
-%R_HOME% .\lib\AnnualReport.r %ANNUAL_REPORT_CONFIG% > %ANNUAL_REPORT_LOG% 2>&1
+%R_HOME% --vanilla -f .\lib\AnnualReport.r %ANNUAL_REPORT_CONFIG% > %ANNUAL_REPORT_LOG% 2>&1
 .\bin\tail %ANNUAL_REPORT_LOG%
 
 start .\report\2015_AnnualReport.html
--- 2016_Create_Annual_Report.bat
+++ 2016_Create_Annual_Report.bat
@@ -5,7 +5,7 @@ call ./bin/setup
 set ANNUAL_REPORT_LOG=".\log\2016_Annual_Report.log"
 set ANNUAL_REPORT_CONFIG=".\config\2016_report_config.r"
 
-%R_HOME% .\lib\AnnualReport.r %ANNUAL_REPORT_CONFIG% > %ANNUAL_REPORT_LOG% 2>&1
+%R_HOME% --vanilla -f .\lib\AnnualReport.r %ANNUAL_REPORT_CONFIG% > %ANNUAL_REPORT_LOG% 2>&1
 .\bin\tail %ANNUAL_REPORT_LOG%
 
 start .\report\2016_AnnualReport.html
