77ddb4f12d8959a5e632f9f4f35f1a5ef0949e94
Nicholas Komick <nicholas.komick@dfo-mpo.gc.ca>
2018-01-31 22:54:38 -0800

    Use latest version of R
    CDFO IT is pushing out R version 3.4.3

--- bin/setup.bat
+++ bin/setup.bat
@@ -12,3 +12,4 @@ if exist "C:\Program Files\R\R-3.2.3\bin\i386\R.exe" set R_HOME="C:\Program File
 if exist "C:\Program Files\R\R-3.2.4\bin\i386\R.exe" set R_HOME="C:\Program Files\R\R-3.2.4\bin\i386\R.exe"
 if exist "C:\Program Files\R\R-3.2.5\bin\i386\R.exe" set R_HOME="C:\Program Files\R\R-3.2.5\bin\i386\R.exe"
 if exist "C:\Program Files\R\R-3.3.3\bin\i386\R.exe" set R_HOME="C:\Program Files\R\R-3.3.3\bin\i386\R.exe"
+if exist "C:\Program Files\R\R-3.4.3\bin\i386\R.exe" set R_HOME="C:\Program Files\R\R-3.4.3\bin\i386\R.exe"
