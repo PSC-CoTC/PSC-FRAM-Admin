ec0398ada7fac141a28f2650f1cdb20652a4a617
Nicholas Komick <nicholas.komick@dfo-mpo.gc.ca>
2018-02-02 08:42:09 -0800

    fix passing cmd line parameters
    Added the "--args" identify the command line argument to pass to the R
    script

--- 2014_Create_Annual_Report.bat
+++ 2014_Create_Annual_Report.bat
@@ -5,7 +5,7 @@ call ./bin/setup
 set ANNUAL_REPORT_LOG=".\log\2014_Annual_Report.log"
 set ANNUAL_REPORT_CONFIG=".\config\2014_report_config.r"
 
-%R_HOME% --vanilla -f .\lib\AnnualReport.r %ANNUAL_REPORT_CONFIG% > %ANNUAL_REPORT_LOG% 2>&1
+%R_HOME% --vanilla -f .\lib\AnnualReport.r --args %ANNUAL_REPORT_CONFIG% > %ANNUAL_REPORT_LOG% 2>&1
 .\bin\tail %ANNUAL_REPORT_LOG%
 
 start .\report\2014_AnnualReport.html
