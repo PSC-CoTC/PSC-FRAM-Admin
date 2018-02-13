c9168718b3cb23438709d5d740f2bed6ad981e41
methiess <m_thiess@hotmail.com>
2018-02-13 08:08:11 -0800

    Update 2016_report_config.r
    Removed old code used for testing

--- config/2016_report_config.r
+++ config/2016_report_config.r
@@ -8,4 +8,3 @@ post.season.tamm <- "./fram db/TAMM_Files_Postseason/coho BK 2015 Final Feb 15th
 pre.season.fram.db <- "./fram db/Final pre and post databases/2018PFMC_NOF_ForPSC-Coho-PreSeason-thru2016_compact.mdb"
 pre.season.run.name <- "bc-Coho1637 Final"
 pre.season.tamm <- "./fram db/TAMM_Files_Preseason/Coho1637_FinalPFMCOcean_MayNOF_052616rv_wMultiYrFix.xlsm"
-#pre.season.tamm <- "./fram db/TAMM_Files_Postseason/coho BK 2015 Final Feb 15th.xlsm"  # this works to produce annual report
