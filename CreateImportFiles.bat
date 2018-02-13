7a5d94cd1c93e17671ed772e56df6d418b9d09d2
Nicholas Komick <nicholas.komick@dfo-mpo.gc.ca>
2018-02-13 15:23:42 -0800

    fix calls to R in command line scripts

--- CreateImportFiles.bat
+++ CreateImportFiles.bat
@@ -7,7 +7,7 @@ if not exist ".\log" mkdir .\log
 set CREATE_IMPORT_LOG=".\log\create_import_files.log"
 set CREATE_IMPORT_CONFIG=".\config\create_import_config.r"
 
-%R_HOME% .\lib\CreateImportFiles.r %CREATE_IMPORT_CONFIG% > %CREATE_IMPORT_LOG% 2>&1
+%R_HOME% --vanilla -f .\lib\CreateImportFiles.r --args %CREATE_IMPORT_CONFIG% > %CREATE_IMPORT_LOG% 2>&1
 .\bin\tail %CREATE_IMPORT_LOG%
 
 pause
--- ExportFramFisheryData.bat
+++ ExportFramFisheryData.bat
@@ -5,7 +5,7 @@ call ./bin/setup
 set EXPORT_FILE_LOG=".\log\export_fram_fishery_data.log"
 set EXPORT_FILE_CONFIG=".\config\export_fram_fishery_config.r"
 
-%R_HOME% .\lib\ExportFramFisheryData.r %EXPORT_FILE_CONFIG% > %EXPORT_FILE_LOG% 2>&1
+%R_HOME% --vanilla -f .\lib\ExportFramFisheryData.r --args %EXPORT_FILE_CONFIG% > %EXPORT_FILE_LOG% 2>&1
 .\bin\tail -n 30 %EXPORT_FILE_LOG%
 
 pause
