ddc142596d1e0bfd9a85b7e2f9a6d1e9c5e2bab8
methiess <m_thiess@hotmail.com>
2018-02-13 08:05:41 -0800

    added --vanilla -f to %R_HOME%

--- ImportPostSeasonFile.bat
+++ ImportPostSeasonFile.bat
@@ -5,7 +5,7 @@ call ./bin/setup
 set IMPORT_FILE_LOG=".\log\import_post_season_file.log"
 set IMPORT_FILE_CONFIG=".\config\import_post_season_config.r"
 
-%R_HOME% .\lib\ImportPostSeasonFile.r %IMPORT_FILE_CONFIG% > %IMPORT_FILE_LOG% 2>&1
+%R_HOME% --vanilla -f .\lib\ImportPostSeasonFile.r %IMPORT_FILE_CONFIG% > %IMPORT_FILE_LOG% 2>&1
 .\bin\tail -n 30 %IMPORT_FILE_LOG%
 
 pause
