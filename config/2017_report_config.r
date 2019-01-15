0c38284550823d5217bc12b61a91de211c162257
methiess <m_thiess@hotmail.com>
2019-01-15 10:18:39 -0800

    Add files via upload
    - added Periodic Report comparison config files (2010-2016)
    - added 2017 Annual Report config file

---
+++ config/2017_report_config.r
@@ -0,0 +1,14 @@
+
+run.year <- 2017
+
+post.season.fram.db <- "./fram db/Final pre and post databases/FramVS2-PSC-Coho-Backwards-thru2016.mdb"
+post.season.run.name <- ""
+post.season.tamm <- "./fram db/TAMM_Files_Postseason/.xlsx"
+post.season.tamm.fishery.ref <- "./data/TammFisheryQueetsRef.csv"
+post.season.tamm.esc.ref <- "./data/TammEscQueetsRef.csv"
+
+pre.season.fram.db <- "./fram db/Final pre and post databases/US_PFMC_NOF_FinalCohoFRAM_MultiYr.mdb"
+pre.season.run.name <- "bkCoho1731"
+pre.season.tamm <- "./fram db/TAMM_Files_Preseason/Coho1731_Final_wSht1Mtrx.xlsx"
+pre.season.tamm.fishery.ref <- "./data/TammFisheryFullRef.csv"
+pre.season.tamm.esc.ref <- "./data/TammEscFullRef.csv"
