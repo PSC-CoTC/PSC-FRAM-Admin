55d76f43e82a2e09b195f7d19b3d7eeec9793b90
methiess <m_thiess@hotmail.com>
2019-01-29 15:17:04 -0800

    Update 2013_PR_config.r

--- config/2013_PR_config.r
+++ config/2013_PR_config.r
@@ -1,4 +1,4 @@
-
+#note: here "pre.season" means "original BK post-season" for the Periodic Report comparison tables
 run.year <- 2013
 
 post.season.fram.db <- "./fram db/PeriodicReportdb/FramVS2-PSC-Coho-Backwards-redo 2010-2016 January 2019 products.mdb"
@@ -9,6 +9,6 @@ post.season.tamm.esc.ref <- "./data/TammEscQueetsRef.csv"
 
 pre.season.fram.db <- "./fram db/PeriodicReportdb/US_PFMC_NOF_FinalCohoFRAM_MultiYr.mdb"
 pre.season.run.name <- "bc-1323"
-pre.season.tamm <- "./fram db/PeriodicReportdb/old2010-2016TAMMfiles/Final Coho1323_wMultiYrFix.xlsm"
-pre.season.tamm.fishery.ref <- "./data/TammFisheryFullRef.csv"
-pre.season.tamm.esc.ref <- "./data/TammEscFullRef.csv"
+#pre.season.tamm <- ""
+#pre.season.tamm.fishery.ref <- ""
+#pre.season.tamm.esc.ref <- ""
