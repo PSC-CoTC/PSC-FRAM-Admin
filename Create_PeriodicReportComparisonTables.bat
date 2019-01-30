6135171311b886f648a6e3b2721d28c9c038d54a
methiess <m_thiess@hotmail.com>
2019-01-29 16:06:53 -0800

    Update Create_PeriodicReportComparisonTables.bat

--- Create_PeriodicReportComparisonTables.bat
+++ Create_PeriodicReportComparisonTables.bat
@@ -2,9 +2,9 @@ REM echo off
 
 call ./bin/setup
 
-set PERIODIC_REPORT_LOG=".\log\PerRep.log"
+set PERIODIC_REPORT_LOG=".\log\PeriodicReportLog.txt"
 
-%R_HOME% --vanilla -f .\lib\PeriodicReport.r --args  %PERIODIC_REPORT_LOG% 2>&1
+%R_HOME% --vanilla -f .\lib\PeriodicReport.r --args > %PERIODIC_REPORT_LOG% 2>&1
 .\bin\tail %PERIODIC_REPORT_LOG%
 
 for %%G in (.\report\20*.html) do (
