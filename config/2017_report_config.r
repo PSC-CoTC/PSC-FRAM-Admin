4bb0406bbad8aef0b591dd5e006c752fedda2a54
methiess <m_thiess@hotmail.com>
2019-02-14 08:27:02 -0800

    Add files via upload

---
+++ config/2017_report_config.r
@@ -0,0 +1,14 @@
+
+run.year <- 2017
+
+post.season.fram.db <- "./fram db/Final pre and post databases/FramVS2-PSC-Coho-Backwards-redo 2010-2016 January 2019 products with BK2017 take2.mdb"
+post.season.run.name <- "bc-BK Coho 2017 final take2"
+post.season.tamm <- "./fram db/TAMM_Files_Postseason/BK Coho2017__Sht1Mtrx step 2 final take2.xlsx"
+post.season.tamm.fishery.ref <- "./data/TammFisheryQueetsRef.csv"
+post.season.tamm.esc.ref <- "./data/TammEscQueetsRef.csv"
+
+pre.season.fram.db <- "./fram db/Final pre and post databases/US_PFMC_NOF_FinalCohoFRAM_MultiYr_PreSeason-thru2018.mdb"
+pre.season.run.name <- "bc-Coho1731_Final"
+pre.season.tamm <- "./fram db/TAMM_Files_Preseason/Coho1731_Final_wSht1Mtrx.xlsx"
+pre.season.tamm.fishery.ref <- "./data/TammFisheryFullRef.csv"
+pre.season.tamm.esc.ref <- "./data/TammEscFullRef.csv"
