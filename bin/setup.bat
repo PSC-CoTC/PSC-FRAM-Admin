6f0b2114387f328d83a43fcdfaa6c7046de579bf
Nicholas Komick <nicholas.komick@dfo-mpo.gc.ca>
2020-01-13 15:03:41 -0800

    add version 3.6.1 of R

--- bin/setup.bat
+++ bin/setup.bat
@@ -2,3 +2,4 @@ if exist "C:\Program Files\R\R-3.5.0\bin\i386\R.exe" set R_HOME="C:\Program File
 if exist "C:\Program Files\R\R-3.5.1\bin\i386\R.exe" set R_HOME="C:\Program Files\R\R-3.5.1\bin\i386\R.exe"
 if exist "C:\Program Files\R\R-3.5.2\bin\i386\R.exe" set R_HOME="C:\Program Files\R\R-3.5.2\bin\i386\R.exe"
 if exist "C:\Program Files\R\R-3.6.0\bin\i386\R.exe" set R_HOME="C:\Program Files\R\R-3.6.0\bin\i386\R.exe"
+if exist "C:\Program Files\R\R-3.6.1\bin\i386\R.exe" set R_HOME="C:\Program Files\R\R-3.6.1\bin\i386\R.exe"
