8c6f516f6c72e92ce9fedf88dd8779245eafc8a8
Mike O'Brien <michael.o'brien@dfo-mpo.gc.ca>
2020-02-12 15:21:54 -0800

    Added comment to CNR data

--- sql/InsertFramNonRetention.sql
+++ sql/InsertFramNonRetention.sql
@@ -1,2 +1,2 @@
-INSERT INTO NonRetention (RunID, FisheryID, TimeStep, NonRetentionFlag, CNRInput1, CNRInput2, CNRInput3, CNRInput4) 
-VALUES (%RUNID%, %FISHERYID%, %TIMESTEP%, 1, %CNRMORTALITIES%, 0, 0, 0);
+INSERT INTO NonRetention (RunID, FisheryID, TimeStep, NonRetentionFlag, CNRInput1, CNRInput2, CNRInput3, CNRInput4, Comment) 
+VALUES (%RUNID%, %FISHERYID%, %TIMESTEP%, 1, %CNRMORTALITIES%, 0, 0, 0, %comment%);
