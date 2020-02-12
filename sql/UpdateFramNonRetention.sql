439849a890d35c45696caed5e385c1cd797f707f
Mike O'Brien <michael.o'brien@dfo-mpo.gc.ca>
2020-02-12 15:23:01 -0800

    Add comment to CNR

--- sql/UpdateFramNonRetention.sql
+++ sql/UpdateFramNonRetention.sql
@@ -1,2 +1,4 @@
-UPDATE NonRetention SET NonRetentionFlag=1, CNRInput1=%CNRMORTALITIES%
+UPDATE NonRetention SET NonRetentionFlag=1, 
+CNRInput1=%CNRMORTALITIES%,
+comment = %comment%
 WHERE RunID=%RUNID% AND FisheryID=%FISHERYID% AND TimeStep=%TIMESTEP%;
