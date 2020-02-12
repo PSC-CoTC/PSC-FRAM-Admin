df57514e068445c48ec33142b8ca47eb9f100ed1
Mike O'Brien <michael.o'brien@dfo-mpo.gc.ca>
2020-02-12 15:10:20 -0800

    Added R 3.6.1 to the setup.bat

--- bin/setup.bat
+++ bin/setup.bat
@@ -1,5 +1,9 @@
 if exist "C:\Program Files\R\R-3.5.0\bin\i386\R.exe" set R_HOME="C:\Program Files\R\R-3.5.0\bin\i386\R.exe"
+
 if exist "C:\Program Files\R\R-3.5.1\bin\i386\R.exe" set R_HOME="C:\Program Files\R\R-3.5.1\bin\i386\R.exe"
+
 if exist "C:\Program Files\R\R-3.5.2\bin\i386\R.exe" set R_HOME="C:\Program Files\R\R-3.5.2\bin\i386\R.exe"
+
 if exist "C:\Program Files\R\R-3.6.0\bin\i386\R.exe" set R_HOME="C:\Program Files\R\R-3.6.0\bin\i386\R.exe"
+
 if exist "C:\Program Files\R\R-3.6.1\bin\i386\R.exe" set R_HOME="C:\Program Files\R\R-3.6.1\bin\i386\R.exe"
